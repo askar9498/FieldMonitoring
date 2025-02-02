5ec7357bc0920c7fc7f781c828e32de26cf7c6c5
khazaei210 <siavash.khazaei2023@gmail.com>
2025-02-02 19:26:24 +0330

    add tubular models to admin.py file

--- apps/tubular/admin.py
+++ apps/tubular/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
+from .models import TubularCatalogue, WellTube
+from import_export.admin import ImportExportModelAdmin
+from unfold.contrib.import_export.forms import ExportForm, ImportForm, SelectableFieldsExportForm
 
-# Register your models here.
+@admin.register(TubularCatalogue)
+class TubularCatalogueAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
+
+@admin.register(WellTube)
+class WellTubeAdmin(ModelAdmin, ImportExportModelAdmin):
+    import_form_class = ImportForm
+    export_form_class = ExportForm
